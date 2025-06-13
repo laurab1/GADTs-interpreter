e407a355a1ae3de423c1b22f4771f56886a289f7
laurab1 <l.bussi1@studenti.unipi.it>
2025-06-13 19:00:50 +0200

    add binding

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,19 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, ExistentialQuantification, DeriveDataTypeable #-}
 module Main where
+import Data.Typeable
 
 data Value =
     VInt Int | VBool Bool deriving (Eq, Show)
 
-type Env = [(String, Value)]
+data ValueWrapper where
+    ValueWrapper :: Typeable a => Simple a -> ValueWrapper
+
+type Env = [(String, ValueWrapper)]
 
 data Simple a where
     IVal :: Int -> Simple Int
     IBool :: Bool -> Simple Bool
+    IVar :: String -> Simple a
     IAdd :: Simple Int -> Simple Int -> Simple Int
     IMul :: Simple Int -> Simple Int -> Simple Int
     IDiv :: Simple Int -> Simple Int -> Simple Int
@@ -16,10 +21,15 @@ data Simple a where
     If :: Simple Bool -> Simple a -> Simple a -> Simple a
     Let :: String -> Simple a -> Simple a -> Simple a
 
-evalSimple :: Simple a -> Env -> Either String a
+evalSimple :: Typeable a => Simple a -> Env -> Either String a
 evalSimple expr env = case expr of
     IVal n -> Right n
     IBool b -> Right b
+    IVar x -> case lookup x env of
+        Nothing -> Left ("Unbound variable" ++ x)
+        Just (ValueWrapper v) -> case cast v of
+            Nothing -> Left "Type mismatch"
+            Just e -> evalSimple e env
     IAdd n m -> do
         v1 <- evalSimple n env
         v2 <- evalSimple m env
@@ -41,19 +51,14 @@ evalSimple expr env = case expr of
             Left "Error: division by zero"
     If b e1 e2 -> do
         guard <- evalSimple b env
-        if guard then do
-            v1 <- evalSimple e1 env
-            return v1
-        else do
-            v2 <- evalSimple e2 env
-            return v2
-    Let x e1 e2 -> let v1 = evalSimple e1 env
-                       env2 = (x,v1):env
-                   in evalSimple e2 env2
+        if guard then evalSimple e1 env else evalSimple e2 env
+    Let x e1 e2 -> let env2 = (x, ValueWrapper e1):env in evalSimple e2 env2
+
 
 main :: IO ()
 main = do
-    _ <- print (evalSimple (IDiv (IVal 5) (IVal 0)))
+    _ <- print (evalSimple (IDiv (IVal 5) (IVal 0)) [])
     -- _ <- print (evalSimple (IsZero (IsZero (IVal 3))))
-    _ <- print (evalSimple (If (IBool False) (IVal 3) (IVal 4)))
+    _ <- print (evalSimple (If (IBool False) (IVal 3) (IVal 4)) [])
+    _ <- print (evalSimple (Let "x" (IVal 3) (IAdd (IVar "x") (IVal 5))) [])
     return ()
